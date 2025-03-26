04a92ddf3874ebbd9c3a1f54d01169953dd26d81
Opilane TTHK <nikitalitvinenko28@gmail.com>
2025-03-26 09:54:14 +0200

    update

--- main.js
+++ main.js
@@ -2,5 +2,13 @@ console.log("Hello")
 
 function openPage(page)
 {
-    window.location.href = page
+    fetch(page)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('content').innerHTML = data;
+    })
+    .catch(error => {
+        document.getElementById('content').innerHTML = `<p>Sorry, an error occurred while loading the page.</p>`;
+        console.error('Error loading page:', error);
+    });
 }
